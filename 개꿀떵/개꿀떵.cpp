c903078b1d817b7cd086efda3c7ebb53e22d3f39
zwnmu <zwnmu@DESKTOP-GDCKK46>
2021-07-17 20:05:16 +0900

    초기 커밋.

---
+++ 개꿀떵/개꿀떵.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	char b;
+	int c;
+	int ab;
+	int z = 1;
+
+	cin >> a;
+	cin >> b;
+	cin >> c;
+
+	while(b != 'e')
+	{
+	
+		if (z != 1)
+		{
+			cin >> a;
+			cin >> b;
+			cin >> c;
+		}
+
+		if (b == '+')
+		{
+			ab = a + c;
+		}
+		else if (b == '-')
+		{
+			ab = a - c;
+		}
+		else if (b == '*')
+		{
+			ab = a * c;
+		}
+		else if (b == '/')
+		{
+			ab = a / c;
+		}
+		else if (b != 'e')
+		{
+			cout << "error";
+			return 0;
+		}
+		else
+		{
+			return 0;
+		}
+		z += 1;
+		cout << ab;
+	}
+	return 0;
+}
